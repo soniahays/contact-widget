c65ed7bcb8a9ba29c5c2059888089a432cba4282
Sonia Brahmi <sonia.brahmi@lightspeedpos.com>
2015-07-27 21:52:12 -0400

    Updated Express deprecated functions
    - MethodOverride
    - bodyParser
    - errorHandler
    - configure

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
 'use strict';
 
-var express = require("express"),
-    _ = require("underscore"),
-    app     = express(),
-    port    = parseInt(process.env.PORT, 10) || 8080,
-    contacts = require('./data/contacts.json');
+var express = require('express'),
+	_ = require('underscore'),
+	app     = express(),
+	bodyParser = require('body-parser'),
+	errorHandler = require('errorhandler'),
+	hostname = process.env.HOSTNAME || 'localhost',
+	methodOverride = require('method-override'),
+	port    = parseInt(process.env.PORT, 10) || 8080,
+	publicDir = __dirname + '/public',
+	contacts = require('./data/contacts.json');
 
-app.get("/", function(req, res) {
-    res.redirect("/index.html");
+app.get('/', function(req, res) {
+	res.redirect("/index.html");
 });
 
-app.get("/contacts", function(req, res) {
-    res.json(contacts.contacts);
+app.get('/contacts', function(req, res) {
+	res.json(contacts.contacts);
 });
 
-app.get("/contact/:id", function(req, res) {
-    console.log(contacts.contacts);
-    res.json(_.findWhere(contacts.contacts, {id: parseInt(req.params.id)}));
+app.get('/contact/:id', function(req, res) {
+	console.log(contacts.contacts);
+	res.json(_.findWhere(contacts.contacts, {id: parseInt(req.params.id)}));
 });
 
-app.configure(function(){
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
-    app.use(app.router);
-});
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+	dumpExceptions: true,
+	showStack: true
+}));
 
-app.listen(port);
+app.use(app.router);
+
+app.listen(port, hostname);
 
 
