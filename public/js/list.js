ad161b97538f730d4dab5b368a641aef7422a2f2
Sonia Brahmi <sonia.brahmi@lightspeedpos.com>
2015-08-02 13:14:52 -0400

    WIP: Folder Owls animation

--- public/js/list.js
+++ public/js/list.js
@@ -13,23 +13,23 @@ angular.module('lightSpeedSonia.controllers')
             var id = $scope.contacts.length;
             $scope.contacts.push({id: id, firstName: "", lastName: "", jobTitle: "", phoneNumber: "", picture: ""});
             $scope.currentContact = $scope.contacts[id];
-        }
+        };
 
         $scope.removeContact = function () {
             $scope.contacts = $filter('filter')($scope.contacts, {id: '!' + $scope.currentContact.id});
-        }
+        };
 
         $scope.setCurrentContact = function (index) {
             $scope.currentContact = $scope.contacts[index];
-        }
+        };
 
         $scope.edit = function () {
             $scope.isEditMode = true;
-        }
+        };
 
         $scope.endEdit = function () {
             $scope.isEditMode = false;
-        }
+        };
 
     }]);
 
